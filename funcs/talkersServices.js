681952d1545472ba3f1b6dfeee972404ffed50de
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-08-02 19:08:26 -0300

    feat: Adiciona função de requisição de palestrantes.

---
+++ funcs/talkersServices.js
@@ -0,0 +1,14 @@
+const fs = require('fs').promises;
+
+const getTalkers = async () => {
+  const talkers = await fs.readFile('./talker.json', 'utf8');
+  if (JSON.parse(talkers).length) return JSON.parse(talkers);
+  return [];
+};
+
+const sendTalkers = async (_request, response) => {
+  const talkers = await getTalkers();
+  response.status(200).send(talkers);
+};
+
+module.exports = { sendTalkers };
